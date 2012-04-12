b1967c7f97abce28fbcd7d48650fc8da60b4eb40
Dan Caragea <dancaragea@gmail.com>
2012-04-12 20:52:58 +0300

    - pagination (work in progress)

--- js/sco.pagination.js
+++ js/sco.pagination.js
@@ -43,37 +43,38 @@
 		,count_out: 3
 		,cssclass: ''
 		,onInit: function(options) {
-			var data = {
-				current_page: options.active
-				,cssclass: options.cssclass
-				,total_pages: 0
-			};
+			var pagination_template = _.template(
+					'<ul>' +
+					'	<li class="prev <%= current_page === 0 ? "disabled" : "" %>">' +
+					'		<a href="#">&larr; Previous</a>' +
+					'	</li>' +
+					'	<% for (var i=0; i<total_pages; i++) {' +
+					'		var cssclass = "";' +
+					'		if (i == current_page) {' +
+					'			cssclass += "active";' +
+					'		}' +
+					//'		if (content == "&hellip;") {' +
+					//'			cssclass += " disabled";' +
+					//'		}' +
+					'		print("<li class=\""+cssclass+"\"><a href=\"#"+i+"\">"+(i+1)+"</a></li>");' +
+					'	} %>' +
+					'	<li class="next <%= next_page === false ? "disabled" : "" %>">' +
+					'		<a href="#">Next &rarr;</a>' +
+					'	</li>' +
+					'</ul>'
+				)
+				,data = {
+					current_page: options.active
+					,cssclass: options.cssclass
+					,total_pages: 0
+				};
 			data.total_pages = this.$content.length;
-			this.$tabs.html(pagination_template(data));
+			if (data.total_pages > 1) {
+				this.$tabs.html(pagination_template(data));
+			}
 		}
 		,onSelect: function() {
 
 		}
 	});
-
-	var pagination_template = _.template(
-		'<ul>' +
-		'	<li class="prev <%= previous_page === false ? "disabled" : "" %>">' +
-		'		<a href="#">&larr; Previous</a>' +
-		'	</li>' +
-		'	<% _.each(links, function(number, content)) {' +
-		'		var cssclass = "";' +
-		'		if (number == current_page) {' +
-		'			cssclass += "active";' +
-		'		}' +
-		'		if (content == "&hellip;") {' +
-		'			cssclass += " disabled";' +
-		'		}' +
-		'		print("<li class=\""+cssclass+"\"><a href=\"#"+number+"\">"+content+"</a></li>");' +
-		'	} %>' +
-		'	<li class="next <%= next_page === false ? "disabled" : "" %>">' +
-		'		<a href="#">Next &rarr;</a>' +
-		'	</li>' +
-		'</ul>'
-	);
 }));
