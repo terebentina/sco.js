28db791de67b60c2f2ca0c58ec32bbd68c4232c1
Dan Caragea <dancaragea@gmail.com>
2012-04-25 17:37:33 +0300

    - ease functions instead of css3 transitions

--- js/sco.collapse.js
+++ js/sco.collapse.js
@@ -51,6 +51,12 @@
 	$.extend(Collapse.prototype, {
 		toggle: function() {
 			var self = this;
+
+			//self.$target.toggleClass(self.options.activeTargetClass);
+			self.$target[$.camelCase(self.options.ease + '-toggle')]();
+			if (self.$parent && !self.$trigger.hasClass(self.options.activeTriggerClass)) {
+				self.$parent.find(self.options.triggerSelector + '.' + self.options.activeTriggerClass).scollapse();
+			}
 			self.$trigger.toggleClass(self.options.activeTriggerClass);
 			if (self.options.triggerHtml !== null) {
 				self.$trigger.html(function() {
@@ -60,12 +66,6 @@
 						return self.options.triggerHtml.off;
 					}
 				});
-			}
-
-			self.$target.toggleClass(self.options.activeTargetClass);
-			if (self.$parent && self.$trigger.hasClass(self.options.activeTriggerClass)) {
-				// since we know for sure all triggers are inited in an accordion
-				self.$parent.find(self.options.triggerSelector + '.' + self.options.activeTriggerClass).not(self.$trigger).scollapse();
 			}
 		}
 	});
@@ -88,14 +88,15 @@
 		parent: null						// having a parent activates the accordion mode behaviour
 		,target: null						// the element to show/hide. If null, the target is chosen based on the "mode" selector
 		,activeTriggerClass: 'active'		// class to add to the trigger in active (on) state
-		,activeTargetClass: 'in'			// class to add to the target in active (on) state
+		//,activeTargetClass: 'in'			// class to add to the target in active (on) state
 		,triggerHtml: null					// if not null, this should be a hash like {off: 'more', on: 'less'}. This text is set on the trigger.
 		,mode: 'next'						// "next" means target is after trigger, "prev" means target is before trigger
 		,collapseSelector: '.collapse'		// used in accordion to find out what to collapse when the current target expands or if the target is null
 		,triggerSelector: '[data-trigger="collapse"]'		// used in accordion to find out all triggers
+		,ease: 'slide'						// the animation effect to use. Must support toggle (like slideToggle/fadeToggle or even empty string :))
 	};
 
-	$(document).on('click.sco', '[data-trigger="collapse"]', function(e) {
+	$(document).on('click.scollapse', '[data-trigger="collapse"]', function(e) {
 		$(this).scollapse({triggerSelector: e.handleObj.selector});
 		if ($(this).is('a')) {
 			return false;
