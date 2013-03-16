e3f1eb4d94421c3eb8811d819bf285adae762861
Dan Caragea <dancaragea@gmail.com>
2013-03-16 17:45:58 +0200

    started tab refactoring

--- js/sco.tab.js
+++ js/sco.tab.js
@@ -18,31 +18,60 @@
  * ========================================================== */
 
 /*jshint laxcomma:true, sub:true, browser:true, jquery:true, devel:true */
-/*global define:true */
 
-(function(factory) {
+;(function($, undefined) {
 	"use strict";
 
-    if (typeof define === 'function' && define.amd) {
-        // Register as an anonymous AMD module:
-        define([
-            'jquery'
-        ], factory);
-    } else {
-        // Browser globals:
-        factory(window.jQuery);
-    }
-}(function($) {
-	"use strict";
+	var pluginName = 'scojs_tab';
 
-	function Tab($tabs, options) {
-		var self = this;
+	function Tab($tab_headers, options) {
+		this.options = $.extend({}, $.fn[pluginName].defaults, options);
+		this.$tab_headers = $tab_headers;
 
-		$.extend(self, {
-			options: {},
-			$tabs: null,
-			$content: null,
+		if (typeof this.options.content === 'undefined') {
+			return;
+		}
+		this.$content = $(this.options.content).children();
 
+		if (typeof this.options.onInit === 'function') {
+			this.options.onInit.call(this);
+		}
+
+		this.$tab_headers.on('click.' + pluginName, 'a', function(e) {
+			var $this = $(this)
+				,$my_li = $this.parents('li')
+				,my_index = $my_li.index()
+				,options = {active: my_index};
+
+			if (!$.address) {
+				e.preventDefault();
+			}
+			// @todo not working
+			//if ($this.attr('href').indexOf('#') !== 0) {
+			//	e.preventDefault();
+			//	options.href = $this.attr('href');
+			//}
+
+			this.set(options);
+		});
+
+		this.set(this.options);
+
+		if ($.address) {
+			var self = this;
+			$.address.externalChange(function(e) {
+				var hash = '#' + e.value.slice(1);
+				this.$tab_headers.find('a').each(function(i) {
+					if ($(this).attr('href') === hash) {
+						self.set({active: i});
+						return false;
+					}
+				});
+			}).history(true);
+		}
+	}
+
+	$.extend(Tab.prototype, {
 			get: function(key) {
 				if (typeof self.options[key] !== 'undefined') {
 					return self.options[key];
@@ -51,17 +80,15 @@
 				return undefined;
 			},
 
-			set: function(map, save) {
-				if (typeof save === 'undefined') {
-					save = true;
-				}
+			set: function(map) {
+				var self = this;
 				$.each(map, function(k, v) {
 					if (k === 'active') {
 						if (v !== self.get('active')) {
 							if (typeof self.options.onBeforeSelect == 'function') {
 								self.options.onBeforeSelect.call(self, v);
 							}
-							var $header_children = self.$tabs.children();
+							var $header_children = self.$tab_headers.children();
 							// remove the .active class from all tab headers and add .active to selected tab header
 							$header_children.removeClass('active').eq(v).addClass('active');
 							// hide all tab content and show only the selected one
@@ -71,96 +98,37 @@
 								}
 							});
 						}
-					//} else if (k === 'href' && v !== '') {
-					//	// @todo not working
-					//	self.$content.find('.active').load(v);
-					}
-
-					if (save) {
-						self.options[k] = v;
+					/*
+					} else if (k === 'href' && v !== '') {
+						// @todo not working
+						self.$content.find('.active').load(v);
+					*/
 					}
 				});
 				return self;
 			},
 
-			_init: function() {
-				self.$tabs = $tabs;
-				var data = self.$tabs.data();
-				if (typeof data.content === 'undefined') {
-					return;
-				}
-				self.$content = $(data.content).children();
-
-				data = $.extend({}, $.fn.scotab.defaults, data, options);
-
-				self.$tabs.on('click.scotab', 'a', function(e) {
-					var $this = $(this)
-						,$my_li = $this.parents('li')
-						,my_index = $my_li.index()
-						,map = {active: my_index};
-
-					if (!$.address) {
-						e.preventDefault();
-					}
-					// @todo not working
-					//if ($this.attr('href').indexOf('#') !== 0) {
-					//	e.preventDefault();
-					//	map.href = $this.attr('href');
-					//}
-
-					self.set(map);
-				});
-
-				// allow plugins to add their own custom init code. Note that self.$tabs and self.$content is available in onInit()
-				if (typeof data.onInit === 'function') {
-					data.onInit.call(self, data);
-					delete data.onInit;
-				}
-				if (typeof data.onBeforeSelect === 'function') {
-					self.options.onBeforeSelect = data.onBeforeSelect;
-					delete data.onBeforeSelect;
-				}
-				if (typeof data.onAfterSelect === 'function') {
-					self.options.onAfterSelect = data.onAfterSelect;
-					delete data.onAfterSelect;
-				}
-
-				// finally, set the initial values
-				self.set(data);
-
-				if ($.address) {
-					$.address.externalChange(function(e) {
-						var hash = '#' + e.value.slice(1);
-						self.$tabs.find('a').each(function(i) {
-							if ($(this).attr('href') === hash) {
-								self.set({active: i});
-								return false;
-							}
-						});
-					}).history(true);
-				}
-
-				return self;
-			}
 		});
-
-		self._init();
 	}
 
 
-	$.fn.scotab = function(options) {
-		if (typeof options === 'undefined') {
-			options = {};
-		}
-		this.each(function() {
-			$(this).data("scotab", new Tab($(this), options));
+	$.fn[pluginName] = function(options) {
+		return this.each(function() {
+			var obj;
+			if (!(obj = $.data(this, pluginName))) {
+				var $this = $(this)
+					,data = $this.data()
+					;
+				options = $.extend({}, $.fn[pluginName].defaults, options, data);
+				obj = new Tab($this, options);
+				$.data(this, pluginName, obj);
+			}
+			obj.toggle();
 		});
-
-		return this;
 	};
 
 
-	$.fn.scotab.defaults = {
+	$.fn[pluginName].defaults = {
 		active: 0
 		,onBeforeSelect: null
 		,onAfterSelect: null
@@ -168,4 +136,4 @@
 		//,easing: null
 		//,href: ''
 	};
-}));
+})(jQuery);
